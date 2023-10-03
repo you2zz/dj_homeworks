4e42c98badbc4159c99c38c8ce7f7219847a91aa
you2zz <yri_vla@mail.ru>
2023-10-03 16:56:51 +0500

    correct meta

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -3,6 +3,17 @@ from django.contrib import admin
 from .models import Article, Tag, Scope
 
 
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    extra = 5
+
+
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'title', 'text', 'published_at', 'image']
+    list_filter = ['published_at']
+    inlines = [ScopeInline, ]
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -1,12 +1,24 @@
 from django.db import models
 
 
-class Article(models.Model):
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Раздел'
+        verbose_name_plural = 'Разделы'
+        ordering = ['name']
+
+
+class Article(models.Model):
     title = models.CharField(max_length=256, verbose_name='Название')
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
-    image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    image = models.ImageField(null=True, blank=True, verbose_name='Изображение', )
+    tags = models.ManyToManyField(Tag, related_name='tags', through='Scope')
 
     class Meta:
         verbose_name = 'Статья'
@@ -17,12 +29,11 @@ class Article(models.Model):
         return self.title
 
 
-class Tag(models.Model):
-    name = models.CharField(max_length=50)
-    articles = models.ManyToManyField(Article, related_name='tags', through='Scope')
-
-
 class Scope(models.Model):
     article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes')
-    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name='scopes')
-    is_main = models.BooleanField()
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name='scopes', verbose_name='Раздел')
+    is_main = models.BooleanField(verbose_name='Основной')
+
+    class Meta:
+        verbose_name_plural = 'Тематики статьи'
+        ordering = ['-is_main', 'tag__name']
