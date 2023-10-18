3a7013a98b3bc7b9a2cfa883d4908717cec56d29
you2zz <yri_vla@mail.ru>
2023-10-18 15:51:12 +0500

    1550_181023

--- 3.2-crud/stocks_products/logistic/views.py
+++ 3.2-crud/stocks_products/logistic/views.py
@@ -1,16 +1,20 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.viewsets import ModelViewSet
 
 from logistic.models import Product, Stock
 from logistic.serializers import ProductSerializer, StockSerializer
 
-
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    # при необходимости добавьте параметры фильтрации
+    filter_backends = [SearchFilter,]
+    search_fields = ['title', 'description']
 
 
 class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
-    # при необходимости добавьте параметры фильтрации
+    filter_backends = [DjangoFilterBackend, SearchFilter,]
+    filterset_fields = ['products']
+    search_fields = ['products__title', 'products__description']
